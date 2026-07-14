72885d1b9f6fa4e7c9babd21fb7d5f4605af71ce
bodegacentralpintana-ctrl <bodegacentralpintana@gmail.com>
2026-07-14 11:47:12 -0400

    Actualización de credenciales Firebase

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
+  apiKey: "AIzaSyBQchb1v5NX2sp2s0YVRMd2v_snVt8WVjI",
   authDomain: "bodegacentral-24dd9.firebaseapp.com",
   projectId: "bodegacentral-24dd9",
-  storageBucket: "bodegacentral-24dd9.appspot.com",
+  storageBucket: "bodegacentral-24dd9.firebasestorage.app",
   messagingSenderId: "282089103551",
   appId: "1:282089103551:web:49ba28a3a1d90c774d660a"
 };
